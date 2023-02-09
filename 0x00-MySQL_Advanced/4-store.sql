5fc417b0aec40637c9324499ed6ae46dcab89b9a
Chalo1996 <emusyoka759@gmail.com>
2023-02-10 02:45:11 +0300

    Creates a trigger

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,7 +7,7 @@ DELIMITER $$
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders FOR EACH ROW
 BEGIN
-  UPDATE items SET quantity = quantity  - NEW.quantity
+  UPDATE items SET quantity = quantity  - NEW.number
   WHERE items.name = NEW.item_name;
 END$$
 DELIMITER ;
