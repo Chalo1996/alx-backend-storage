63837889267282a2fb14c53119cd8ba7a664b0b6
Chalo1996 <emusyoka759@gmail.com>
2023-02-10 03:29:47 +0300

    trigger before

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,8 @@
 -- resets the attribute valid_email only when the email has been changed.
 
--- DROP TRIGGER IF EXISTS reset_valid_email;
+DROP TRIGGER IF EXISTS reset_valid_email;
 CREATE TRIGGER reset_valid_email
 BEFORE INSERT ON users
 FOR EACH ROW
 UPDATE users SET email = users.valid_email
-WHERE email NOT LIKE valid_email;
+WHERE email NOT valid_email;
