b99abb2642c382c0202a8675f236f8a1d6fcb08c
Chalo1996 <emusyoka759@gmail.com>
2023-02-16 02:02:26 +0300

    Creates an instance of redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import IO
+from typing import BinaryIO
 
 
 class Cache:
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: IO[bytes]) -> str:
+    def store(self, data: BinaryIO) -> str:
         """store method."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
