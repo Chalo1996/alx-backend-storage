7b96dcce1774506ae0b945c62153f490ccb27df6
Chalo1996 <emusyoka759@gmail.com>
2023-02-27 15:05:53 +0300

    Parse binary int to int object and binary str to str object

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,104 +4,26 @@
 
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Optional
 
 
 class Cache:
-    """Cache class."""
-
+    """Simulates the Caching of redis keys. Creates a new connection\
+        everytime an instance of this class is created, saves the\
+            keys temporalily, and deletes the keys when the execution of\
+                the instance stops.
+    """
     def __init__(self):
-        """init method."""
+        """Instantiates a new connection to redis. Flushes the redis\
+            server once connection terminates.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        """count_calls: takes a Callable argument named method
-        and returns a Callable.
-
-        Args:
-            method (Callable): method.
-
-        Returns:
-            Callable: method.
-        """
-        key = method.__qualname__
-
-        def wrapper(self, *args, **kwargs):
-            """wrapper: takes a self argument and returns a
-            Callable.
-
-            Args:
-                self (Callable): method.
-
-            Returns:
-                Callable: method.
-            """
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-
-        return wrapper
-
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store: takes a data argument and returns a string.
-
-        Args:
-            data (bytes): data to store.
-
-        Returns:
-            str: key.
+        """Generates a random key and stores the data in the redis\
+            server.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-        return (key)
-
-    def get(self,
-            key: str,
-            fn: Callable = None) -> Union[str,
-                                          bytes,
-                                          int,
-                                          float]:
-        """get: takes a key string argument and an optional
-        Callable argument named fn.
-
-        Args:
-            key (str): key.
-            fn (callable, optional): function. Defaults to None.
-
-        Returns:
-            Union[str, bytes, int, float]: data.
-        """
-        data = self._redis.get(key)
-        if fn:
-            return fn(data)
-        return data
-
-    def get_str(self, key: str) -> str:
-        """get_str: takes a key string argument and returns the\
-        string value stored in Redis.
-
-        Args:
-            key (str): key.
-
-        Returns:
-            str: data.
-        """
-        if key:
-            data = self._redis.get(key)
-        return data.decode('utf-8')
-
-    def get_int(self, key: str) -> int:
-        """get_int: takes a key string argument and returns the\
-        integer value stored in Redis.
-
-        Args:
-            key (str): key.
-
-        Returns:
-            int: data.
-        """
-        if key:
-            data = self._redis.get(key)
-        return int(data.decode('utf-8'))
+        return key
